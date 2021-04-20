481460efc58e53484c7e79671c3683147f0a8b6d
JDavidLpez723 <75070648+JDavidLpez723@users.noreply.github.com>
2021-04-20 11:23:34 +0200

    login fragment

--- app/src/main/java/com/canonicalexamples/tearank/view/LoginFragment.kt
+++ app/src/main/java/com/canonicalexamples/tearank/view/LoginFragment.kt
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.canonicalexamples.tearank.R
+import com.canonicalexamples.tearank.app.MapApp
+import com.canonicalexamples.tearank.databinding.FragmentHistoryBinding
+import com.canonicalexamples.tearank.databinding.FragmentLoginBinding
+import com.canonicalexamples.tearank.util.observeEvent
+import com.canonicalexamples.tearank.viewmodels.HistoryViewModel
+import com.canonicalexamples.tearank.viewmodels.LoginViewModel
+import com.canonicalexamples.tearank.viewmodels.LoginViewModelFactory
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +27,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel: LoginViewModel by viewModels {
+        val app = activity?.application as MapApp
+        LoginViewModelFactory(app.database)
     }
 
     override fun onCreateView(
@@ -35,26 +39,29 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.LoginBLoginFragment.setOnClickListener {
+            //viewModel.addButtonClicked1()
+        }
+        binding.RegisterBLoginFragment.setOnClickListener {
+            //viewModel.addButtonClicked2()
+        }
+
+     /*   viewModel.go_to_main_fragment.observeEvent(viewLifecycleOwner) {
+            if(it) {
+                findNavController().navigate(R.id.action_MainFragment_to_HistoryFragment)
             }
+        }
+        viewModel.go_to_reg_fragment.observeEvent(viewLifecycleOwner) { navigate ->
+            if(it) {
+                findNavController().navigate(R.id.action_MainFragment_to_HistoryFragment)
+            }
+        }*/
     }
 }
